a385475014d3886e9a6cd07a36a28a0070c89c47
linyao926 <huynhcam227@gmail.com>
2023-04-05 12:13:20 +0700

    Install customize-cra and babel-plugin-module-resolver

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Build Spotify web page
         </a>
       </header>
     </div>
